2a36570afed8058b8d256cb571dd2cdd73835cae
Reynolds-Liu <reynoldsliu01443@gmail.com>
2024-09-21 17:18:31 +0800

    fix: conflicts

--- order-module/src/main/java/com/eight/order/module/controller/OrderController.java
+++ order-module/src/main/java/com/eight/order/module/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.eight.order.module.controller;
 import com.eight.order.module.service.OrderService;
 import com.eight.order.module.model.Order;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +20,8 @@ public class OrderController {
     }
 
     @GetMapping
-    @Operation(summary = "Get an order by ID", description = "Returns an order based on the provided ID")
-    public ResponseEntity<Order> getUserById(@RequestParam("orderId") Integer orderId) {
+    @Operation(summary = "訂單編號拿訂單", description = "ddddd")
+    public ResponseEntity<Order> getOrderById(@RequestParam("orderId") Integer orderId) {
         return ResponseEntity.ok(orderService.getOrderById(orderId).orElse(null));
     }
 }
--- order-module/src/main/java/com/eight/order/module/model/Order.java
+++ order-module/src/main/java/com/eight/order/module/model/Order.java
@@ -4,18 +4,26 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @Entity
-@Table(name = "ORDER")
+@Table(name = "ORDERS")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ORDER_ID")
-    private Integer userId;
+    private Integer orderId;
+
+    @Column(name = "CUSTOMER_ID")
+    private Integer customerId;
+
+    @Column(name = "ORDER_DATE")
+    private Date OrderDate;
 
     @Column(name = "ORDER_DATA")
-    private String userName;
+    private String OrderData;
 
 }
