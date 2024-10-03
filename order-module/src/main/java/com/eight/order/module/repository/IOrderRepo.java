3a706a23291bd95f425ece1505bf0ee58c8e1a94
Reynolds-Liu <reynoldsliu01443@gmail.com>
2024-10-03 16:18:57 +0800

    chore: order-module order's CRUD

--- order-module/src/main/java/com/eight/order/module/repository/IOrderRepo.java
+++ order-module/src/main/java/com/eight/order/module/repository/IOrderRepo.java
@@ -2,9 +2,13 @@ package com.eight.order.module.repository;
 
 import com.eight.order.module.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface IOrderRepo extends JpaRepository<Order, Integer> {
+public interface IOrderRepo extends JpaRepository<Order, String> {
+
 
 }
