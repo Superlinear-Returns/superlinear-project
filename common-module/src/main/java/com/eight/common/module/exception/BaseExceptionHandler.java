3353c4497011773c949b01f3208b13dc3a847a2b
louis.y <louis.y.2.tcg@gmail.com>
2024-10-12 16:23:14 +0800

    feat: create common-module for common exception handler

--- common-module/src/main/java/com/eight/common/module/exception/BaseExceptionHandler.java
+++ common-module/src/main/java/com/eight/common/module/exception/BaseExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.eight.common.module.exception;
 
+import com.eight.common.module.constant.StatusCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class BaseExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseExceptionResponseTO> handleExceptionToResponse(Exception e) {
+        BaseExceptionResponseTO baseExceptionResponseTO = new BaseExceptionResponseTO(StatusCode.UNKNOW_ERR, e.getMessage());
+        return new ResponseEntity<>(baseExceptionResponseTO, HttpStatus.OK);
+    }
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<BaseExceptionResponseTO> handleBaseExceptionToResponse(BaseException e) {
